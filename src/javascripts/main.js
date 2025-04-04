4fc698fa934a3ef7487ede2bac7571428f57bbc8
ash-3939 <ashleymedell@mail.weber.edu>
2025-04-03 22:29:36 -0600

    finished about me

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,9 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".dropdown-toggle").addEventListener("click", function (event) {
+    if (!this.parentElement.querySelector(".dropdown-menu").classList.contains("show")) {
+      window.location.href = this.href
+    }
+  })
+})
