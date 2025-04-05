ebbb680d3cdbd7a178b8024d6a26e9a579462b2c
ash-3939 <ashleymedell@mail.weber.edu>
2025-04-05 14:04:46 -0600

    Final

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,7 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+import * as emailjs from "https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".dropdown-toggle").addEventListener("click", function (event) {
     if (!this.parentElement.querySelector(".dropdown-menu").classList.contains("show")) {
@@ -7,3 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   })
 })
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.getElementById("contact-form").addEventListener("submit", function (e) {
+//     e.preventDefault()
+//     var templateParams = {
+//       name: this.querySelector("[name='fullName']")?.value || "",
+//       email: this.querySelector("[name='email']")?.value || "",
+//       phone: this.querySelector("[name='phone']")?.value || "",
+//       message: this.querySelector("[name='message']")?.value || "",
+//     }
+//     console.log(templateParams)
+
+//     emailjs.send("service_i1pjxrm", "template_p2y7vhf", templateParams).then(
+//       () => {
+//         alert("✅ Message sent successfully!")
+//         this.reset() // clear form
+//       },
+//       (error) => {
+//         console.error("FAILED...", error)
+//         alert("❌ Something went wrong. Try again.")
+//       }
+//     )
+//   })
+// })
