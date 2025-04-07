f2176797a7a6a1e8c648dccdc55f315d848c7d38
ash-3939 <ashleymedell@mail.weber.edu>
2025-04-06 18:52:22 -0600

    fix

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,46 +1,30 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
-import * as emailjs from "https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
-
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".dropdown-toggle").addEventListener("click", function (event) {
-    if (!this.parentElement.querySelector(".dropdown-menu").classList.contains("show")) {
-      window.location.href = this.href
-    }
-  })
+  const dropdownToggle = document.querySelector(".dropdown-toggle")
+
+  if (dropdownToggle) {
+    dropdownToggle.addEventListener("click", function (event) {
+      const dropdownMenu = this.parentElement.querySelector(".dropdown-menu")
+
+      if (dropdownMenu && !dropdownMenu.classList.contains("show")) {
+        const href = this.getAttribute("href")
+        if (href) {
+          window.location.href = href
+        }
+      }
+    })
+  }
 })
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("downloadBtn")
 
   button.addEventListener("click", function () {
     const link = document.createElement("a")
-    link.href = "./images/Resume.pdf" // or any file type
-    link.download = "Resume.pdf" // optional
+    link.href = "/resume-ashley-medell.pdf" // or any file type
+    link.download = "/resume-ashley-medell.pdf" // optional
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
   })
 })
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.getElementById("contact-form").addEventListener("submit", function (e) {
-//     e.preventDefault()
-//     var templateParams = {
-//       name: this.querySelector("[name='fullName']")?.value || "",
-//       email: this.querySelector("[name='email']")?.value || "",
-//       phone: this.querySelector("[name='phone']")?.value || "",
-//       message: this.querySelector("[name='message']")?.value || "",
-//     }
-//     console.log(templateParams)
-
-//     emailjs.send("service_i1pjxrm", "template_p2y7vhf", templateParams).then(
-//       () => {
-//         alert("✅ Message sent successfully!")
-//         this.reset() // clear form
-//       },
-//       (error) => {
-//         console.error("FAILED...", error)
-//         alert("❌ Something went wrong. Try again.")
-//       }
-//     )
-//   })
-// })
